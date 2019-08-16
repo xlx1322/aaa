94769cc814f5dc11b0a5981ea1fda376210d0e6a
xlx1322 <52655492+xlx1322@users.noreply.github.com>
2019-08-16 11:34:43 +0800

    Update led.c

--- led.c
+++ led.c
@@ -3,8 +3,8 @@
 void LED_init(void)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
-	RCC_APB1PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
-	RCC_APB1PeriphClockCmd(RCC_APB2Periph_GPIOE,ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE,ENABLE);
 	
 	
 	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_Out_PP;
